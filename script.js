322200347fe76d28e34dab49089b57878b5cac7c
CometCoder192 <96585650+CometCoder192@users.noreply.github.com>
2024-09-22 23:53:12 +0530

    Add html boilerplate and link all links.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Project Layout - 
+// 1. make a layout in html using css grid preferably. make sure it's responsive later by adding media queries
+// 2. after styling the page, do the simple tasks first, like linking the dom with the page using lets and vars.
+// 3. after that, make a simple reduce func which accumulates the addition and display it to the console.
+// any and every thing can be done after we successfully manange to do operations on the numbers submitted by the user.
+// 4. after updating state every time at the display window, add other functionality to 
